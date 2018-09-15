adba61799b5aa18d858914bd34509d46dcf0a404
Matheus Compri <matheus.compri@gmail.com>
2018-09-14 21:26:00 -0300

    feat: adding animation to results component

--- components/QuizResults.js
+++ components/QuizResults.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View  } from 'react-native';
+import { StyleSheet, Text, View, Animated  } from 'react-native';
 
 import Button from '../components/Button'
 
-export default QuizResults = (props) => {
-  const percentage = Math.ceil((props.correct / props.total) * 100);
-  return (
-    <View style={styles.resultsContainer}>
-      <View style={styles.results}>
-        <Text style={styles.message}>You got</Text>
-        <Text style={styles.percentage}>{percentage}%</Text>
-        <Text style={styles.message}>correct!</Text>
-      </View>
-      <View>
-        <Button label='Restart Quiz' onPress={props.restartQuiz}></Button>
-        <View style={styles.cardActions}>
-          <Button label='Back' onPress={props.handleBackButton}></Button>
-          <Button label='Home' onPress={props.handleBackToHome}></Button>
+export default class QuizResults extends Component {
+
+  state = {
+    opacity: new Animated.Value(0)
+  }
+
+  componentDidMount() {
+    const { opacity } = this.state;
+
+    Animated.timing(opacity, { toValue: 1, velocity: 2 }).start();
+  }
+
+  render() {
+    const { opacity } = this.state;
+    const percentage = Math.ceil((this.props.correct / this.props.total) * 100);
+    return (
+      <Animated.View style={[styles.resultsContainer, { opacity }]}>
+        <View style={styles.results}>
+          <Text style={styles.message}>You got</Text>
+          <Text style={styles.percentage}>{percentage}%</Text>
+          <Text style={styles.message}>correct!</Text>
         </View>
-      </View>
-    </View>
-  );
+        <View>
+          <Button label='Restart Quiz' onPress={this.props.restartQuiz}></Button>
+          <View style={styles.cardActions}>
+            <Button label='Back' onPress={this.props.handleBackButton}></Button>
+            <Button label='Home' onPress={this.props.handleBackToHome}></Button>
+          </View>
+        </View>
+      </Animated.View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
